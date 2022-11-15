a9a4df0b70793701c5a67f6c9313f6d6f7c88e29
Oline <olinep@live.dk>
2022-11-15 19:35:56 +0100

    startet projektet op ud fra kickstarter og oprettet database

--- index.php
+++ index.php
@@ -1,8 +1,43 @@
+<!DOCTYPE html>
+
+<html lang="da">
+
+<head>
+    <meta charset="utf-8">
+
+    <title>Sigende titel</title>
+
+    <meta name="robots" content="All">
+    <meta name="author" content="Udgiver">
+    <meta name="copyright" content="Information om copyright">
+
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+    <script src="https://kit.fontawesome.com/b2cd0c4e29.js" crossorigin="anonymous"></script>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+
 <?php
 require "settings/init.php";
 
-$blogs = $db->sql("SELECT * FROM blogs");
+$boger = $db->sql("SELECT * FROM boger");
 
-foreach ($blogs as $blog){
-    echo $blog->blogName . "<br>";
+foreach ($boger as $bog){
+    ?>
+    <div class="col-3">
+        <?php echo $bog->prodName; ?>
+        <?php echo $bog->prodDato; ?>
+        <?php echo $bog->prodForfatter; ?>
+
+    </div>
+
+
+<?php
 }
+?>
+
+<script src="node_modules/bootstrap/dist/js//bootstrap.bundle.min.js"></script>
+</body>
+</html>
