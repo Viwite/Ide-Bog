6c0f495ee82efc75afa482383505fb409531120c
cakao <stin915n@edu.zealand.dk>
2022-11-17 17:51:39 +0100

    ændring i index og product side

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     <meta name="author" content="Udgiver">
     <meta name="copyright" content="Information om copyright">
 
-    <link href="css/styles.css" rel="stylesheet" type="text/css">
+    <link href="css/styles.scss" rel="stylesheet" type="text/css">
     <script src="https://kit.fontawesome.com/b2cd0c4e29.js" crossorigin="anonymous"></script>
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -19,25 +19,51 @@
 
 <body>
 
-<?php
-require "settings/init.php";
+    <div class="container">
 
-$boger = $db->sql("SELECT * FROM boger");
+        <div class="boger">
+            <div class="filter p-5">
+                <div class="row">
+                    <div class="col-md-4 offset-md-4">
+                        <input type="search" class="form-control nameSearch" placeholder="Søg og du skal finde">
+                    </div>
+                </div>
+            </div>
+        </div>
 
-foreach ($boger as $bog){
-    ?>
-    <div class="col-3">
-        <?php echo $bog->prodName; ?>
-        <?php echo $bog->prodDato; ?>
-        <?php echo $bog->prodForfatter; ?>
+        <div class="items">
+            <!--Produkterne-->
+        </div>
 
     </div>
 
+    <?php
+    require "settings/init.php";
 
-<?php
-}
-?>
+    $boger = $db->sql("SELECT * FROM boger");
+
+    foreach ($boger as $bog){
+        ?>
+        <div class="col-3">
+            <?php echo $bog->prodName; ?>
+            <?php echo $bog->prodDato; ?>
+            <?php echo $bog->prodForfatter; ?>
+
+        </div>
+
+
+        <?php
+    }
+    ?>
+
+
 
 <script src="node_modules/bootstrap/dist/js//bootstrap.bundle.min.js"></script>
+<script type="module">
+    import Products from "./js/products.js";
+
+    const products = new Products();
+    products.init();
+</script>
 </body>
 </html>
--- product.php
+++ product.php
@@ -11,10 +11,34 @@
     <meta name="author" content="Udgiver">
     <meta name="copyright" content="Information om copyright">
 
-    <link href="css/styles.css" rel="stylesheet" type="text/css">
+    <link href="css/styles.scss" rel="stylesheet" type="text/css">
+    <link href="css/bootstrap.scss" rel="stylesheet" type="text/css">
     <script src="https://kit.fontawesome.com/b2cd0c4e29.js" crossorigin="anonymous"></script>
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
 </head>
 
-<?php
+<body>
+
+    <div class="container">
+
+        <div class="boger">
+            <div class="filter p-5">
+                <div class="row">
+                    <div class="col-md-4 offset-md-4">
+                        <input type="search" class="form-control nameSearch" placeholder="Søg og du skal finde">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="items">
+            <!--Produkterne-->
+        </div>
+
+    </div>
+
+</body>
