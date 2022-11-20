647e0229614864d52b4f61144e3bb8fb6273cac0
Oline <olinep@live.dk>
2022-11-20 16:42:25 +0100

    farvet

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
-<footer class="container-fluid bg-light" ">
+<footer class="container-fluid" style="background: #c5a78d" ">
 
-    <div class="footer row" style="background: #e2ddd6">
+    <div class="footer row" style="background: #c5a78d">
         <div class="col-12 col-lg mb-5">
             <a class="navbar-brand class-text" href="index.php">Ide&Bog</a>
         </div>
--- includes/navigation.php
+++ includes/navigation.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg bg-light">
+<nav class="navbar navbar-expand-lg" style="background: #c5a78d">
     <div class="container-fluid">
         <a class="navbar-brand class-text" href="index.php" ">Ide&Bog</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
