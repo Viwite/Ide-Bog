97e38510541d5aad690fb4647128c45a27970343
cakao <stin915n@edu.zealand.dk>
2022-11-20 15:14:27 +0100

    sat navigation og footer i includes

---
+++ includes/footer.php
@@ -0,0 +1,22 @@
+<footer class="container-fluid bg-light">
+
+    <div class="footer row">
+        <div class="col-12 col-lg mb-5">
+            <a class="navbar-brand" href="index.php">Ide&Bog</a>
+        </div>
+        <div class="footer-text col-12 col-lg text-center mt-5 mb-5" style="color: #4a2700">
+            <p>Shopping</p>
+            <p>Gifts</p>
+            <p>Events</p>
+            <p>Special Edition</p>
+        </div>
+        <div class="footer-text col-12 col-lg text-center mt-5 mb-5" style="color: #4a2700">
+            <p>Contact Us</p>
+            <p>Returns</p>
+            <p>FAQ</p>
+            <p>Terms of privacy</p>
+            <p>Get help</p>
+        </div>
+    </div>
+
+</footer>
--- index.php
+++ index.php
@@ -20,27 +20,7 @@
 
 <body>
 
-<nav class="navbar navbar-expand-lg bg-light">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="index.php">Ide&Bog</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">About</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Shop</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Contact</a>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>
+<?php include "includes/navigation.php"; ?>
 
 <div class="container-fluid">
 
@@ -60,28 +40,9 @@
 
 </div>
 
-<footer class="container-fluid bg-light">
+<?php include "includes/footer.php"; ?>
 
-    <div class="footer row">
-        <div class="col-12 col-lg mb-5">
-            <a class="navbar-brand" href="index.php">Ide&Bog</a>
-        </div>
-        <div class="footer-text col-12 col-lg text-center mt-5 mb-5" style="color: #4a2700">
-            <p>Shopping</p>
-            <p>Gifts</p>
-            <p>Events</p>
-            <p>Special Edition</p>
-        </div>
-        <div class="footer-text col-12 col-lg text-center mt-5 mb-5" style="color: #4a2700">
-            <p>Contact Us</p>
-            <p>Returns</p>
-            <p>FAQ</p>
-            <p>Terms of privacy</p>
-            <p>Get help</p>
-        </div>
-    </div>
 
-</footer>
 
 <?php
 require "settings/init.php";
